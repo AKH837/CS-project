1f29646836f6edf9a5042deb21e12cad7fc4bb6e
AKH837 <166520784+AKH837@users.noreply.github.com>
2024-10-05 20:54:43 +0530

    Delete gd.py

--- gd.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-
-my_file = “https://drive.google.com/file/d/1KC7NdwwmH6-Puit1MYxPoONxjkDejw1R/view?usp=sharing”
-video_file = open(my_file, “rb”).read()
-show_topics.video(video_file
